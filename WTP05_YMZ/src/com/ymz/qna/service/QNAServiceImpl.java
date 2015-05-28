fa6a7b4c1a69453601b996dd649f2d36a0987e2b
an-in-seek <dktmrorl@naver.com>
2015-05-28 09:05:44 +0900

    QNAMapper.xml : 수정 FAQMapper.xml : 수정 QNADAOImpl : 수정 FAQDAOImple : 수정

--- WTP05_YMZ/src/com/ymz/qna/service/QNAServiceImpl.java
+++ WTP05_YMZ/src/com/ymz/qna/service/QNAServiceImpl.java
@@ -17,33 +17,39 @@ public class QNAServiceImpl implements QNAService {
 	@Autowired
 	private QNADAO dao;
 
+	//QNA게시판 글 등록
 	@Override
 	public void registerQNA(QNA qna) {
 		dao.insertQNA(qna);
 	}
 
+	//QNA게시판 글 수정
 	@Override
 	public void modifyQNA(QNA qna) {
 		dao.updateQNA(qna);
 	}
 
+	//QNA게시판 글 제거
 	@Override
 	public void removeQNAByNo(int number) {
 		dao.deleteQNAByNo(number);
 	}
 
+	//QNA게시판 글 전체목록 조회
 	@Override
 	public List<QNA> getQNAList() {
 		List<QNA> list = dao.selectAllQNA();
 		return list;
 	}
 	
+	//QNA게시판 글 번호로 조회
 	@Override
 	public QNA getQNAByNo(int number) {
 		QNA qna = dao.selectQNAByNo(number);
 		return qna;
 	}
 
+	//트랜젝션 테스트
 	@Override
 	@Transactional(rollbackFor={Exception.class}, propagation=Propagation.REQUIRED) //자동 롤백은 uncheck 일 경우만 된다. 그래서 rollbackF
 	public void transactionTest(QNA q1, QNA q2) throws Exception {
